8aaac4c0b3233c608a64b51370edede6597ab70b
Derek Rowland <derek.a.rowland@gmail.com>
2017-04-25 16:59:34 -0500

    initial commit

---
+++ js/sidebar.js
@@ -0,0 +1,33 @@
+document.getElementById("sidenav").innerHTML = 
+	'<br><br><br>' +
+	'<div class="circular--landscape">' +
+		'<img src="images/avatar.jpg" />' +
+	'</div>' +
+	'<h1 class="sidebarheader">Derek Rowland</h1>' +
+	'<h4 class="sidebarheader">Electrical Engineer</h4>' +
+	'<p class="links"><a href="index.html">HOME</a></p>' +
+	'<p class="links"><a href="resume.html">RESUME</a></p>' +
+	'<p class="links"><a href="hobby.html">HOBBY PROJECTS</a></p>' +
+	'<p class="links"><a href="#">ABOUT</a></p>'+
+	'<br><br><br><br><br><br>' +
+	'<div class="sidecontainer">' +
+		'<div class="logo-left">' +
+			'<a href="https://github.com/gx1400/" target="_blank">' +
+				'<img src="images/githublogo.png">' +
+			'</a>' +
+		'</div>' +
+		
+		'<div class="logo-generic">' +
+			'<a href="mailto:derek.a.rowland@gmail.com" target="_blank">' +
+				'<img src="images/email.png">' +
+			'</a>' +
+		'</div>' +
+		
+		'<div class="logo-right">' +
+			'<a href="https://www.linkedin.com/in/derekrowland/" target="_blank">' +
+				'<img src="images/linkedinlogocrop.png">' +
+			'</a>' +
+		'</div>' +
+		
+		
+	'</div>';
