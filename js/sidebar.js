28cadf59a4f3d835a69523cad26decf34e86ca6d
Derek Rowland <Derek Rowland>
2017-04-25 21:21:22 -0500

    changed index.html to / in sidebar nav

--- js/sidebar.js
+++ js/sidebar.js
@@ -5,7 +5,7 @@ document.getElementById("sidenav").innerHTML =
 	'</div>' +
 	'<h1 class="sidebarheader">Derek Rowland</h1>' +
 	'<h4 class="sidebarheader">Electrical Engineer</h4>' +
-	'<p class="links"><a href="index.html">HOME</a></p>' +
+	'<p class="links"><a href="/">HOME</a></p>' +
 	'<p class="links"><a href="resume.html">RESUME</a></p>' +
 	'<p class="links"><a href="hobby.html">HOBBY PROJECTS</a></p>' +
 	'<p class="links"><a href="about.html">ABOUT</a></p>'+
